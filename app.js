4024337b08f94574a0ebf39c76512cef9ba8ecc9
Lucas Andre <lucaslopes05@gmail.com>
2019-07-15 00:12:38 -0300

    add bibl. body-parser e config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 
 
@@ -9,18 +10,20 @@ const handlebars = require('express-handlebars')
     //Tamplete Engine 
         app.engine('handlebars',handlebars({defaultLayout: 'main'}))
         app.set('view engine','handlebars')
+
+    //body-parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())   
+
     //Conecao com banco de dados
     
 
 //rotas
 
+
 //outros 
-
-
-
-
-//numero da posta da aplicacao 
-const POST = 3030
-app.listen(POST, ()=>{
-    console.log("Servidor rodano... ")
-})
+    //numero da posta da aplicacao 
+    const PORT = 3030
+    app.listen(PORT, ()=>{
+        console.log("Servidor rodano... ")
+    })
