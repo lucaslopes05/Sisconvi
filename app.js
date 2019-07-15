5a073e51ffa7a898eccebf0685e388d90cedf3d9
Lucas Andre <lucaslopes05@gmail.com>
2019-07-15 00:51:00 -0300

    Config. o bd Mongoose

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 
 
 
-//configuracoes
+//Configuracoes
     //Tamplete Engine 
         app.engine('handlebars',handlebars({defaultLayout: 'main'}))
         app.set('view engine','handlebars')
@@ -15,13 +16,19 @@ const bodyParser = require('body-parser')
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())   
 
-    //Conecao com banco de dados
+    //Conecao com db Mongoose
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/blogapp',{useNewUrlParser: true}).then(()=>{
+            console.log('Conectado ao Mongo Db')
+        }).catch((err)=>{
+            console.log('Erro no mongoose: '+err);
+        })
     
 
-//rotas
+//Rotas
 
 
-//outros 
+//Outros 
     //numero da posta da aplicacao 
     const PORT = 3030
     app.listen(PORT, ()=>{
